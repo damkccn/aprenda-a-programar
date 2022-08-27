6094a2eb2573632c43bb6717623ccce2bb3860e3
damkccn <dadaduarte8@gmail.com>
2022-08-27 15:12:17 -0300

    upload pratica 1-2

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -4,4 +4,8 @@ let profissao = "estudante"
 let idade = 16
 let sonhoEmVisitar = "Seul - Coreia do Sul"
 
+console.log(nome)
 console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(sonhoEmVisitar)
